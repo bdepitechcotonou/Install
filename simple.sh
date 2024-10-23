6f811454e31ccdb99b1740ed76046f7cc2d533b7
AnalyticAce <shalom.dosseh@epitech.eu>
2024-10-23 10:26:55 +0100

    Update package list and install wget if snap is not installed

--- simple.sh
+++ simple.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
+echo_message() {
+    echo "\e[1;32m$1\e[0m"
+}
+
+echo_message "Updating Package List..."
+sudo apt update
+
+echo_message "Checking and Installing Wget..."
+if ! snap --version > /dev/null 2>&1; then
+    echo_message "snap is not installed. Installing wget..."
+    sudo apt install snapd -y
+else
+    echo_message "snap is already installed."
+fi
+
 sudo snap install discord
